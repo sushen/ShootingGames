9556fe1edf394c4a0fc1c5772c4860f9511acc04
sushen <sushenbiswasaga@gmail.com>
2018-12-29 10:30:06 +0600

    Plain is moving left and right.

--- Play.py
+++ Play.py
@@ -9,7 +9,7 @@ black = (0, 0, 0)
 white = (255, 255, 255)
 red = (255, 0, 0)
 
-gmaeDisolay = pygame.display.set_mode((display_width, display_height))
+gmaeDisplay = pygame.display.set_mode((display_width, display_height))
 
 pygame.display.set_caption('A bit Race')
 
@@ -19,23 +19,40 @@ carImg = pygame.image.load('aroplane.png')
 
 
 def car(x, y):
-    gmaeDisolay.blit(carImg, (x, y))
+    gmaeDisplay.blit(carImg, (x, y))
 
 
 x = (display_width * 0.45)
 y = (display_height * 0.8)
 
+x_change = 0
+
 crashed = False
 
 while not crashed:
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             crashed = True
 
-    gmaeDisolay.fill(white)
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                x_change = -5
+
+            elif event.key == pygame.K_RIGHT:
+                x_change = 5
+
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                x_change = 0
+
+    x += x_change
+
+    gmaeDisplay.fill(white)
     car(x, y)
+
     pygame.display.update()
-    clock.tick(60)
+    clock.tick(30)
 
 pygame.quit()
 quit()
