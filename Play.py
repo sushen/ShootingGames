46e7a72394c7b3eb9f33dab7d9ed0cf3f964ef45
sushen <sushenbiswasaga@gmail.com>
2018-12-29 10:48:42 +0600

    I just define game loop function

--- Play.py
+++ Play.py
@@ -21,38 +21,39 @@ carImg = pygame.image.load('aroplane.png')
 def car(x, y):
     gmaeDisplay.blit(carImg, (x, y))
 
+def game_loop():
+    x = (display_width * 0.45)
+    y = (display_height * 0.8)
 
-x = (display_width * 0.45)
-y = (display_height * 0.8)
+    x_change = 0
 
-x_change = 0
+    crashed = False
 
-crashed = False
+    while not crashed:
 
-while not crashed:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                crashed = True
 
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            crashed = True
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LEFT:
+                    x_change = -5
 
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_LEFT:
-                x_change = -5
+                elif event.key == pygame.K_RIGHT:
+                    x_change = 5
 
-            elif event.key == pygame.K_RIGHT:
-                x_change = 5
+            if event.type == pygame.KEYUP:
+                if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                    x_change = 0
 
-        if event.type == pygame.KEYUP:
-            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
-                x_change = 0
+        x += x_change
 
-    x += x_change
+        gmaeDisplay.fill(white)
+        car(x, y)
 
-    gmaeDisplay.fill(white)
-    car(x, y)
+        pygame.display.update()
+        clock.tick(60)
 
-    pygame.display.update()
-    clock.tick(30)
-
+game_loop()
 pygame.quit()
 quit()
