86733946b9a4b173d82bdfe6012c60ab3d251415
sushen <sushenbiswasaga@gmail.com>
2018-12-29 14:02:10 +0600

    My games just crash and start again.

--- Play.py
+++ Play.py
@@ -1,4 +1,5 @@
 import pygame
+import time
 
 pygame.init()
 
@@ -8,6 +9,8 @@ display_height = 600
 black = (0, 0, 0)
 white = (255, 255, 255)
 red = (255, 0, 0)
+
+car_width = 55
 
 gmaeDisplay = pygame.display.set_mode((display_width, display_height))
 
@@ -21,19 +24,43 @@ carImg = pygame.image.load('aroplane.png')
 def car(x, y):
     gmaeDisplay.blit(carImg, (x, y))
 
+
+def text_object(text, font):
+    textSurface = font.render(text, True, black)
+    return textSurface, textSurface.get_rect()
+
+
+def message_display(text):
+    largeText = pygame.font.Font('freesansbold.ttf', 116)
+    TextSurf, TextRect = text_object(text, largeText)
+    TextRect.center = ((display_width / 2), (display_height / 2))
+    gmaeDisplay.blit(TextSurf, TextRect)
+
+    pygame.display.update()
+
+    time.sleep(2)
+
+    game_loop()
+
+
+def crash():
+    message_display(" You Carah")
+
+
 def game_loop():
     x = (display_width * 0.45)
     y = (display_height * 0.8)
 
     x_change = 0
 
-    crashed = False
+    game_exit = False
 
-    while not crashed:
+    while not game_exit:
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                crashed = True
+                pygame.quit()
+                quit()
 
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_LEFT:
@@ -51,8 +78,12 @@ def game_loop():
         gmaeDisplay.fill(white)
         car(x, y)
 
+        if x > display_width - car_width or x < 0:
+            crash()
+
         pygame.display.update()
         clock.tick(60)
+
 
 game_loop()
 pygame.quit()
